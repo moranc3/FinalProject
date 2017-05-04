179c6a6d83ebe2607ae78f74916e2e02dfd643d6
corri.moran <moranc3@hawkmail.newpaltz.edu>
2017-05-04 17:30:49 -0400

    added enemies

--- Assets/Scripts/StunGrenade.cs
+++ Assets/Scripts/StunGrenade.cs
@@ -1,16 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;	
 using UnityEngine;
 
-public class StunGrenade : MonoBehaviour {
+namespace AssemblyCSharp
+{
 
-	// Use this for initialization
-	void Start () {
+	public class StunGrenade : Throwable {
+
+		public float blastRadius = 5;
+
+		void OnCollisionEnter2D(Collision2D coll)
+		{
+			var player = coll.gameObject.GetComponent<Player>();
+			if(isActive && player == null) {
+				Explode();
+			}
+		}
+
+
+		public void Explode()
+		{
+			//get reference to all enemies
+			var enemies = FindObjectsOfType<Enemy> ();
+
+			// loop through each enemy in the list
+			foreach (var e in enemies) {
+
+				//check if that enemy is within the blast radius
+				if (Vector3.Distance (this.transform.position, e.transform.position) < blastRadius) {
+
+					StartCoroutine(Stun (e));
+				}
+			}
+
+			//set bomb to not active so it disappears and you cant pick it up again.
+			collider2D.enabled = false;
+			GetComponent<SpriteRenderer> ().enabled = false;
+		}
+
+		IEnumerator Stun(Enemy e)
+		{
+			Debug.Log
+			var renderer = e.GetComponent<SpriteRenderer> ();
+			var animator = e.GetComponent<Animator> ();
+
+			e.enabled = false; 
+			if (animator != null) {
+				animator.enabled = false;
+			}
+			for (int i =0; 1 <8; i++) {
+				renderer.color = new Color (1, 1, 1, 1- (i* .1f));
+				yield return new WaitForSeconds (.4f);
+			}
+
+			yield return new WaitForSeconds (5);
 		
-	}
+
+			for (int i =0; 1 <11; i++) {
+				renderer.color = new Color (1, 1, 1, i *.1f);
+				yield return new WaitForSeconds (.4f);
+			}
+
+			if (animator != null) {
+				animator.enabled = true;
+			}		e.enabled = true;
 	
-	// Update is called once per frame
-	void Update () {
-		
 	}
+}
 }
