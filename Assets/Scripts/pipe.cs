4f5a3bda5b298430ef2d834c26c2b5811393243c
corri.moran <moranc3@hawkmail.newpaltz.edu>
2017-05-15 12:11:21 -0400

    FINAL PROJECT

---
+++ Assets/Scripts/pipe.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipe : MonoBehaviour {
+
+	Player player;
+	public bool movePlayer = false;
+	public GameObject destination;
+
+	public void OnCollisionEnter2D (Collision2D coll)
+	{
+		if (Input.GetKeyDown (KeyCode.A)) {
+			movePlayer = true;
+			player.transform.position = destination.transform.position;
+		}
+	}
+
+}
+
